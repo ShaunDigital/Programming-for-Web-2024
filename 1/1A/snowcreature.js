5e018deae4c441e5a97ad1908ae8ccc8fd37c526
Shaun Ritchie <sritchiebass@gmail.com>
2024-01-21 15:32:53 -0500

    1A part 1 complete

--- 1/1A/snowcreature.js
+++ 1/1A/snowcreature.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+let stroke1 = prompt("What color would you like?", "blue");
+let stroke2 = prompt("What color would you like?", "purple");
+let fill1 = prompt("What color would you like?", "red");
+let fill2 = prompt("What color would you like?", "turquoise");
 function setup() {
   createCanvas(1000, 600).position(170, 20);
   background("#99ccff");
@@ -14,22 +18,32 @@ function draw() {
   //head
   fill("#f1f1f1");
   strokeWeight(1);
-  stroke(20, 10, 100);
+  stroke(stroke1);
   ellipse(500, 142, 95);
   //body middle
   strokeWeight(1);
-  stroke(20, 10, 100);
+  stroke(stroke2);
   ellipse(500, 265, 150);
   //body bottom
   strokeWeight(1);
-  stroke(20, 10, 100);
+  stroke(stroke1);
   ellipse(500, 445, 210);
   //hat body
-  fill("#003377");
+  fill(fill1);
   strokeWeight(1);
-  quad(440, 120, 480, 45, 510, 65, 550, 95);
+  quad(450, 120, 450, 70, 510, 55, 530, 90);
   // hat brim
-  fill("#003366");
+  fill(fill2);
   strokeWeight(15);
   line(550, 95, 440, 120);
+  //eyes
+  stroke(0);
+  strokeWeight(20);
+  point(475, 135);
+  point(525, 135);
+  // mouth
+  noFill();
+  strokeWeight(5);
+  arc(495, 160, 50, 30, 0, HALF_PI);
+
 }
