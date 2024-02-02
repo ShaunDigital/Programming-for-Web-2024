31302f361990f54d05ea1538fb15a932bab342ef
Shaun Ritchie <sritchiebass@gmail.com>
2024-02-01 22:31:43 -0500

    Ex 3.2

---
+++ 3.2/pattern.js
@@ -0,0 +1,54 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function createTile(originX, originY) {
+    translate(originX, originY);
+    fill("#9a53d5");
+    rect(0, 0, 200, 200);
+    stroke("#e18e1e");
+    strokeWeight(5);
+    line(200, 200, 50, 0);
+    line(200, 200, 100, 0);
+    line(200, 200, 150, 0);
+    line(200, 200, -10, 0);
+    line(200, 200, -75, 0);
+    line(200, 200, -175, 0);
+    line(200, 200, -375, 0);
+    line(200, 200, -775, 0);
+   
+    
+    
+    line(50, 0, 0, 200);
+    line(100, 0, 0, 200);
+    line(150, 0, 0, 200);
+    line(200, 50, 0, 200);
+    line(200, 80, 0, 200);
+    line(200, 120, 0, 200);
+    line(200, 0, 0, 200);
+    line(200, 150, 0, 200);
+    line(200, 170, 0, 200);
+    noStroke();
+    fill("#cfd553");
+    circle(55, 100, 25);
+    circle(100, 55, 25);
+    circle(100, 100, 40);
+    circle(100, 145, 25);
+    circle(145, 100, 25);
+    noFill();
+    stroke("#1ed2e1");
+    strokeWeight(6);
+    circle(100, 100, 55);
+    circle(55, 100, 65);
+    circle(145, 100, 65);
+    circle(100, 55, 65);
+    circle(100, 145, 65);
+  
+  
+  } 
+  
+  function draw() {
+  createTile();
+  }
+    
+  
