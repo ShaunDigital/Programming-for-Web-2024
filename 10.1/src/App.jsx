cac130498ed3c27456bb3abb91d89627734c366a
Shaun Ritchie <sritchiebass@gmail.com>
2024-03-27 21:02:27 -0400

    10.1 work

--- 10.1/src/App.jsx
+++ 10.1/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./views/Home";
+import { Llamas } from "./views/Llamas";
+import { Alpaca } from "./views/Alpaca";
+import "./App.css";
 
 function App() {
-  
-
   return (
-   <>
-   hi
-   </>
-  )
+    <>
+      {/* Nav here */}
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Llamas">Llamas</Link></li>
+        <li><Link to="/Alpaca">Alpacas</Link></li>
+      </ul>
+      {/*Routes Here*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Llamas" element={<Llamas />} />
+        <Route path="/Alpaca" element={<Alpaca />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
