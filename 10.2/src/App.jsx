5861f0a299b4a7b398dc02c6b8c943d32bab8b59
Shaun Ritchie <sritchiebass@gmail.com>
2024-03-30 20:14:59 -0400

    10.2 done

--- 10.2/src/App.jsx
+++ 10.2/src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import { Link, Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
+import { Song } from "./views/Song";
+import "./App.css";
+import musicData from "./assets/music-list.json";
 
 function App() {
-  
-
   return (
     <>
-      Hi
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path=":slug" element={<Song data={musicData} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
