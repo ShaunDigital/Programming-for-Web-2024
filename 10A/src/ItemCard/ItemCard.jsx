ee59ce35a93ace97a6d47d1f8df84592cd17b377
Shaun Ritchie <sritchiebass@gmail.com>
2024-03-31 18:25:55 -0400

    10A Work

---
+++ 10A/src/ItemCard/ItemCard.jsx
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import "./ItemCard.css";
+import trophy from "../assets/icons/trophy.svg";
+import trash from "../assets/icons/trashcan.svg";
+import copy from "../assets/icons/copy.svg";
+export default function ItemCard({
+  gameTitle,
+  publisher,
+  gameType,
+  completed,
+  platform,
+  image,
+  id,
+  deleteFn,
+  duplicateFn,
+}) {
+  return (
+    <div className="gameCard">
+      {completed && (
+        <div className="completed">
+          <img src={trophy} title=" I have beaten this game" alt="trophy" />{" "}
+        </div>
+      )}
+      <div className="cardImg">
+        <img src={image} alt={gameTitle} />
+      </div>
+      <div className="cardTitle">{gameTitle}</div>
+      <div className="action">
+        <a
+          href="#"
+          onClick={(evt) => {
+            evt.preventDefault();
+            deleteFn(id);
+          }}
+        >
+          <img src={trash} alt="delete" />
+        </a>
+        <a
+          href="#"
+          onClick={(evt) => {
+            evt.preventDefault();
+            duplicateFn(id);
+          }}
+        >
+          <img src={copy} />
+        </a>
+      </div>
+    </div>
+  );
+}
+ItemCard.propTypes = {
+  gameTitle: PropTypes.string,
+  publisher: PropTypes.arrayOf(PropTypes.string),
+  gameType: PropTypes.string,
+  completed: PropTypes.bool,
+  platform: PropTypes.string,
+  image: PropTypes.string,
+  id: PropTypes.string,
+  duplicateFn: PropTypes.func,
+  deleteFn: PropTypes.func,
+};
