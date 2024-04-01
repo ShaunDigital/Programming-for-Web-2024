8590dae6effa9e1ed6faafe4ecc4ed9a10a17edc
Shaun Ritchie <sritchiebass@gmail.com>
2024-04-01 18:48:46 -0400

    10A done

--- 10A/src/App.jsx
+++ 10A/src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./views/Home";
 import { GameDetail } from "./views/GamesDetail";
+import gameData from "./assets/gameData.json";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<GameDetail />} />
+      <Route path="/:id" element={<GameDetail data={gameData} />}  />
     </Routes>
   );
 }
--- 10A/src/views/Home.jsx
+++ 10A/src/views/Home.jsx
@@ -43,7 +43,7 @@ export function Home() {
           })}
         </div>
         <hr />
-        <NewGamesForm />
+        {/*<NewGamesForm />*/}
       </div>
     );
   }
