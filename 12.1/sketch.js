ce2fb9d45fe1eb94ae2b43833c31db1a176d4e7b
Your Name <you@example.com>
2024-04-12 10:36:50 -0400

    pull repo

--- 12.1/sketch.js
+++ 12.1/sketch.js
@@ -1,12 +1,57 @@
+// A Class to describe a "doorbell" (really a button)
+class Doorbell {
+  constructor(x_, y_, r_) {
+    // Location and size
+    this.x = x_;
+    this.y = y_;
+    this.r = r_;
+  }
+  // Is a point inside the doorbell? (used for mouse rollover, etc.)
+  contains(mx, my) {
+    return dist(mx, my, this.x, this.y) < this.r;
+  }
+
+  // Show the doorbell (hardcoded colors, could be improved)
+  display(mx, my) {
+    if (this.contains(mx, my)) {
+      fill(100);
+    } else {
+      fill(175);
+    }
+    stroke(0);
+    strokeWeight(4);
+    ellipseMode(RADIUS);
+    ellipse(this.x, this.y, this.r, this.r);
+  }
+}
+
+// A sound file object
+let dingdong;
+
+// A doorbell object (that will trigger the sound)
+let doorbell;
+
+function preload() {
+  // Load the sound file
+  dingdong = loadSound('assets/doorbell.mp3');
+}
+
 function setup() {
-  createCanvas(1300, 600);
+  createCanvas(200, 200);
+
+  // Create a new doorbell
+  doorbell = new Doorbell(width / 2, height / 2, 32);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(255);
+  // Show the doorbell
+  doorbell.display(mouseX, mouseY);
+}
+
+function mousePressed() {
+  // If the user clicks on the doorbell, play the sound!
+  if (doorbell.contains(mouseX, mouseY)) {
+    dingdong.play();
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
